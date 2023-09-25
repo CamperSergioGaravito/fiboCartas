99e076a4def8b1b15e5f093d57a409de9a669604
CamperSergioGaravito <137243895+CamperSergioGaravito@users.noreply.github.com>
2023-09-25 08:50:00 -0500

    Update poker.js

--- js/poker.js
+++ js/poker.js
@@ -1,85 +1,85 @@
 const as = [
-    'fiboCartas/assets/poker/As_corazon.png',
-    'fiboCartas/assets/poker/As_diamante.png',
-    'fiboCartas/assets/poker/As_picas.png',
-    'fiboCartas/assets/poker/As_trebol.png'
+    '../assets/poker/As_corazon.png',
+    '../assets/poker/As_diamante.png',
+    '../assets/poker/As_picas.png',
+    '../assets/poker/As_trebol.png'
 ];
 
 const dos = [
-    'fiboCartas/assets/poker/2_corazon.png',
-    'fiboCartas/assets/poker/2_diamante.png',
-    'fiboCartas/assets/poker/2_picas.png',
-    'fiboCartas/assets/poker/2_trebol.png'
+    '../assets/poker/2_corazon.png',
+    '../assets/poker/2_diamante.png',
+    '../assets/poker/2_picas.png',
+    '../assets/poker/2_trebol.png'
 ];
 
 const tres = [
-    'fiboCartas/assets/poker/3_corazon.png',
-    'fiboCartas/assets/poker/3_diamante.png',
-    'fiboCartas/assets/poker/3_picas.png',
-    'fiboCartas/assets/poker/3_trebol.png'
+    '../assets/poker/3_corazon.png',
+    '../assets/poker/3_diamante.png',
+    '../assets/poker/3_picas.png',
+    '../assets/poker/3_trebol.png'
 ];
 
 const cuatro = [
-    'fiboCartas/assets/poker/4_corazon.png',
-    'fiboCartas/assets/poker/4_diamante.png',
-    'fiboCartas/assets/poker/4_picas.png',
-    'fiboCartas/assets/poker/4_trebol.png'
+    '../assets/poker/4_corazon.png',
+    '../assets/poker/4_diamante.png',
+    '../assets/poker/4_picas.png',
+    '../assets/poker/4_trebol.png'
 ];
 
 const cinco = [
-    'fiboCartas/assets/poker/5_corazon.png',
-    'fiboCartas/assets/poker/5_diamante.png',
-    'fiboCartas/assets/poker/5_picas.png',
-    'fiboCartas/assets/poker/5_trebol.png'
+    '../assets/poker/5_corazon.png',
+    '../assets/poker/5_diamante.png',
+    '../assets/poker/5_picas.png',
+    '../assets/poker/5_trebol.png'
 ];
 
 const seis = [
-    'fiboCartas/assets/poker/6_corazon.png',
-    'fiboCartas/assets/poker/6_diamante.png',
-    'fiboCartas/assets/poker/6_picas.png',
-    'fiboCartas/assets/poker/6_trebol.png'
+    '../assets/poker/6_corazon.png',
+    '../assets/poker/6_diamante.png',
+    '../assets/poker/6_picas.png',
+    '../assets/poker/6_trebol.png'
 ];
 
 const siete = [
-    'fiboCartas/assets/poker/7_corazon.png',
-    'fiboCartas/assets/poker/7_diamante.png',
-    'fiboCartas/assets/poker/7_picas.png',
-    'fiboCartas/assets/poker/7_trebol.png'
+    '../assets/poker/7_corazon.png',
+    '../assets/poker/7_diamante.png',
+    '../assets/poker/7_picas.png',
+    '../assets/poker/7_trebol.png'
 ];
 
 const ocho = [
-    'fiboCartas/assets/poker/8_corazon.png',
-    'fiboCartas/assets/poker/8_diamante.png',
-    'fiboCartas/assets/poker/8_picas.png',
-    'fiboCartas/assets/poker/8_trebol.png'
+    '../assets/poker/8_corazon.png',
+    '../assets/poker/8_diamante.png',
+    '../assets/poker/8_picas.png',
+    '../assets/poker/8_trebol.png'
 ];
 
 const nueve = [
-    'fiboCartas/assets/poker/9_corazon.png',
-    'fiboCartas/assets/poker/9_diamante.png',
-    'fiboCartas/assets/poker/9_picas.png',
-    'fiboCartas/assets/poker/9_trebol.png'
+    '../assets/poker/9_corazon.png',
+    '../assets/poker/9_diamante.png',
+    '../assets/poker/9_picas.png',
+    '../assets/poker/9_trebol.png'
 ];
 
 const diez = [
-    'fiboCartas/assets/poker/10_corazon.png',
-    'fiboCartas/assets/poker/10_diamante.png',
-    'fiboCartas/assets/poker/10_picas.png',
-    'fiboCartas/assets/poker/10_trebol.png'
+    '../assets/poker/10_corazon.png',
+    '../assets/poker/10_diamante.png',
+    '../assets/poker/10_picas.png',
+    '../assets/poker/10_trebol.png'
 ];
 
 const jota = [
-    'fiboCartas/assets/poker/J_corazon.png',
-    'fiboCartas/assets/poker/J_diamante.png',
-    'fiboCartas/assets/poker/J_picas.png',
-    'fiboCartas/assets/poker/J_trebol.png'
+    '../assets/poker/J_corazon.png',
+    '../assets/poker/J_diamante.png',
+    '../assets/poker/J_picas.png',
+    '../assets/poker/J_trebol.png'
 ];
 
-const cartaAtras = 'fiboCartas/assets/poker/cartaAtras.png';
+const cartaAtras = '../assets/poker/cartaAtras.png';
 
-const cartaFibonacci = 'fiboCartas/assets/poker/carta-fibonacci.png';
+const cartaFibonacci = '../assets/poker/carta-fibonacci.png';
 
-const joker = 'fiboCartas/assets/poker/comodin.png';
+const joker = '../assets/poker/comodin.png';
 
 export const cartas = [
     as,
